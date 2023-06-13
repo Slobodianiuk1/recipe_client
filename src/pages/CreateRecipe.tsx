72ea6109fea9333124e02574b2bf4cdf5e8c50e1
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-06-13 16:15:31 +0300

    feature: add routers and tailwind.css

---
+++ src/pages/CreateRecipe.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const CreateRecipe: FC = () => {
+  return (
+    <div>
+      Create Recipe
+    </div>
+  );
+};
+
+export default CreateRecipe;
---
+++ src/pages/SavedRecipes.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const SavedRecipes:FC = () => {
+  return (
+    <div>
+      Saved Recipes
+    </div>
+  );
+};
+
+export default SavedRecipes;
